70069e78ffbd9b3528209fd5f16d02f1cdf0adbd
wtrst <stwtr0611@gmail.com>
2018-09-14 17:20:00 -0700

    create tipcalculator

--- TipCalculator/TipCalculator/ViewController.cs
+++ TipCalculator/TipCalculator/ViewController.cs
@@ -6,7 +6,6 @@ namespace TipCalculator
 {
     public partial class ViewController : UIViewController
     {
-        
         protected ViewController(IntPtr handle) : base(handle)
         {
             // Note: this .ctor should not contain any initialization logic.
@@ -27,14 +26,47 @@ namespace TipCalculator
         public void calculateTipButton_Click()
         {
             
+
         }
 
         partial void calculateTip(UIButton sender)
         {
             double tipAmount;
+            if(string.IsNullOrEmpty(tipPercentageTextField.Text) || Convert.ToInt16(tipPercentageTextField.Text) == 0){
+                tipAmount = double.Parse(billAmountTextField.Text) * 0.15;
+            }else{
+                double per = Convert.ToDouble(tipPercentageTextField.Text) / 100;
+                tipAmount = double.Parse(billAmountTextField.Text) * per;
+            }
 
-            tipAmount = double.Parse(billAmountTextField.Text) * 0.15;
-            tipAmountLabel.Text = tipAmount.ToString();
+            tipAmountLabel.Text = "Tip amount: $" + tipAmount.ToString();
+        }
+
+        partial void adjustPer(UISlider sender)
+        {
+            tipPercentageTextField.Text = Convert.ToInt16(adjustTipPercentage.Value).ToString();
+
+            double tipAmount;
+            if (!string.IsNullOrEmpty(billAmountTextField.Text))
+            {
+                double per = Convert.ToDouble(tipPercentageTextField.Text) / 100;
+                tipAmount = double.Parse(billAmountTextField.Text) * per;
+
+                tipAmountLabel.Text = "Tip amount: $" + tipAmount.ToString();
+            }
+        }
+
+        partial void changedPer(UITextField sender)
+        {
+            double tipAmount;
+
+            if(!string.IsNullOrEmpty(billAmountTextField.Text)){
+                double per = Convert.ToDouble(tipPercentageTextField.Text) / 100;
+                tipAmount = double.Parse(billAmountTextField.Text) * per;
+
+                tipAmountLabel.Text = "Tip amount: $" + tipAmount.ToString();
+            }
+
         }
     }
 }
--- TipCalculator/TipCalculator/ViewController.designer.cs
+++ TipCalculator/TipCalculator/ViewController.designer.cs
@@ -15,6 +15,10 @@ namespace TipCalculator
     {
         [Outlet]
         [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UISlider adjustTipPercentage { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
         UIKit.UITextField billAmountTextField { get; set; }
 
         [Outlet]
@@ -25,16 +29,37 @@ namespace TipCalculator
         [GeneratedCode ("iOS Designer", "1.0")]
         UIKit.UILabel tipAmountLabel { get; set; }
 
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField tipPercentageTextField { get; set; }
+
+        [Action ("aaa:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void aaa (UIKit.UITextField sender);
+
+        [Action ("adjustPer:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void adjustPer (UIKit.UISlider sender);
+
+        [Action ("bbb:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void bbb (UIKit.UITextField sender);
+
         [Action ("calculateTip:")]
         [GeneratedCode ("iOS Designer", "1.0")]
         partial void calculateTip (UIKit.UIButton sender);
 
-        [Action ("CalculateTipButton_TouchUpInside:")]
+        [Action ("changedPer:")]
         [GeneratedCode ("iOS Designer", "1.0")]
-        partial void CalculateTipButton_TouchUpInside (UIKit.UIButton sender);
+        partial void changedPer (UIKit.UITextField sender);
 
         void ReleaseDesignerOutlets ()
         {
+            if (adjustTipPercentage != null) {
+                adjustTipPercentage.Dispose ();
+                adjustTipPercentage = null;
+            }
+
             if (billAmountTextField != null) {
                 billAmountTextField.Dispose ();
                 billAmountTextField = null;
@@ -48,6 +73,11 @@ namespace TipCalculator
             if (tipAmountLabel != null) {
                 tipAmountLabel.Dispose ();
                 tipAmountLabel = null;
+            }
+
+            if (tipPercentageTextField != null) {
+                tipPercentageTextField.Dispose ();
+                tipPercentageTextField = null;
             }
         }
     }
